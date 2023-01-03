6b5932fbe1d43fe531a861512b1ac8f31776303e
Yukai Huang <yukaihuangtw@gmail.com>
2023-01-03 16:48:43 +0800

    feat: replace axios with ky

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -1,119 +1,106 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosError, AxiosResponse } from 'axios'
-import { User, Note, Team, CreateNoteOptions, GetMe, GetUserHistory, GetUserNotes, GetUserNote, CreateUserNote, GetUserTeams, GetTeamNotes, CreateTeamNote, DeleteUserNote, DeleteTeamNote, UpdateUserNote, SingleNote, UpdateTeamNote } from './type'
+import ky from 'ky-universal'
+
+import { CreateNoteOptions, GetMe, GetUserHistory, GetUserNotes, GetUserNote, CreateUserNote, GetUserTeams, GetTeamNotes, CreateTeamNote, DeleteUserNote, DeleteTeamNote, UpdateUserNote, SingleNote, UpdateTeamNote } from './type'
 import * as HackMDErrors from './error'
 
 export default class API {
-  private axios: AxiosInstance
+  private ky: ReturnType<typeof ky.create>
 
   constructor (readonly accessToken: string, public hackmdAPIEndpointURL: string = "https://api.hackmd.io/v1") {
     if (!accessToken) {
       throw new HackMDErrors.MissingRequiredArgument('Missing access token when creating HackMD client')
     }
 
-    this.axios = axios.create({
-      baseURL: hackmdAPIEndpointURL,
-      headers:{
-        "Content-Type": "application/json",
+    this.ky = ky.create({
+      prefixUrl: hackmdAPIEndpointURL,
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      },
+      hooks: {
+        afterResponse: [
+          async (request, options, response) => {
+            if (!response.ok) {
+              if (response.status >= 500) {
+                throw new HackMDErrors.InternalServerError(
+                  `HackMD internal error (${response.status} ${response.statusText})`,
+                  response.status,
+                  response.statusText,
+                )
+              } else {
+                throw new HackMDErrors.HttpResponseError(
+                  `Received an error response (${response.status} ${response.statusText}) from HackMD`,
+                  response.status,
+                  response.statusText,
+                )
+              }
+            }
+          }
+        ]
       }
     })
-
-    this.axios.interceptors.request.use(
-      (config: AxiosRequestConfig) =>{
-        config.headers!.Authorization = `Bearer ${accessToken}`
-        return config
-      },
-      (err: AxiosError) => {
-        return Promise.reject(err)
-      }
-    )
-
-    this.axios.interceptors.response.use(
-      (response: AxiosResponse) => {
-        return response
-      },
-      async (err: AxiosError) => {
-        if (!err.response) {
-          return Promise.reject(err)
-        }
-
-        if (err.response.status >= 500) {
-          throw new HackMDErrors.InternalServerError(
-            `HackMD internal error (${err.response.status} ${err.response.statusText})`,
-            err.response.status,
-            err.response.statusText,
-          )
-        } else {
-          throw new HackMDErrors.HttpResponseError(
-            `Received an error response (${err.response.status} ${err.response.statusText}) from HackMD`,
-            err.response.status,
-            err.response.statusText,
-          )
-        }
-      }
-    )
   }
 
   async getMe (): Promise<GetMe> {
-    const { data } = await this.axios.get<User>("me")
-    return data
+    return this.ky.get("me").json()
   }
 
   async getHistory (): Promise<GetUserHistory> {
-    const { data } = await this.axios.get<Note[]>("history")
-    return data
+    return this.ky.get("history").json()
   }
 
   async getNoteList (): Promise<GetUserNotes> {
-    const { data } = await this.axios.get<Note[]>("notes")
-    return data
+    return ky.get("notes").json()
   }
 
   async getNote (noteId: string): Promise<GetUserNote> {
-    const { data } = await this.axios.get<SingleNote>(`notes/${noteId}`)
-    return data
+    return ky.get(`notes/${noteId}`).json()
   }
 
   async createNote (options: CreateNoteOptions): Promise<CreateUserNote> {
-    const { data } = await this.axios.post<SingleNote>("notes", options)
-    return data
+    return ky.post("notes", {
+      json: options
+    }).json()
   }
 
   async updateNoteContent (noteId: string, content?: string): Promise<UpdateUserNote> {
-   await this.axios.patch<AxiosResponse>(`notes/${noteId}`, { content })
+   await this.ky.patch(`notes/${noteId}`, { json: { content } })
   }
 
-  async updateNote (noteId: string, options: Partial<Pick<SingleNote, 'content' | 'readPermission' | 'writePermission' | 'permalink'>>): Promise<AxiosResponse> {
-    return await this.axios.patch<AxiosResponse>(`notes/${noteId}`, options)
+  async updateNote (noteId: string, options: Partial<Pick<SingleNote, 'content' | 'readPermission' | 'writePermission' | 'permalink'>>) {
+    return await this.ky.patch(`notes/${noteId}`, {
+      json: options
+    }).json()
   }
 
   async deleteNote (noteId: string): Promise<DeleteUserNote> {
-    await this.axios.delete<AxiosResponse>(`notes/${noteId}`)
+    await this.ky.delete(`notes/${noteId}`)
   }
 
   async getTeams (): Promise<GetUserTeams> {
-    const { data } = await this.axios.get<Team[]>("teams")
-    return data
+    return await this.ky("teams").json()
   }
 
   async getTeamNotes (teamPath: string): Promise<GetTeamNotes> {
-    const { data } = await this.axios.get<Note[]>(`teams/${teamPath}/notes`)
-    return data
+    return this.ky.get(`teams/${teamPath}/notes`).json()
   }
 
   async createTeamNote (teamPath: string, options: CreateNoteOptions): Promise<CreateTeamNote> {
-    const { data } = await this.axios.post<SingleNote>(`teams/${teamPath}/notes`, options)
-    return data
+    return await this.ky.post(`teams/${teamPath}/notes`, {
+      json: options
+    }).json()
   }
 
   async updateTeamNoteContent (teamPath: string, noteId: string, content?: string): Promise<UpdateTeamNote> {
-    await this.axios.patch<AxiosResponse>(`teams/${teamPath}/notes/${noteId}`, { content })
+    await this.ky.patch(`teams/${teamPath}/notes/${noteId}`, { json: { content } }).json()
   }
 
-  async updateTeamNote (teamPath: string, noteId: string, options: Partial<Pick<SingleNote, 'content' | 'readPermission' | 'writePermission' | 'permalink'>>): Promise<AxiosResponse> {
-    return await this.axios.patch<AxiosResponse>(`teams/${teamPath}/notes/${noteId}`, options)
+  async updateTeamNote (teamPath: string, noteId: string, options: Partial<Pick<SingleNote, 'content' | 'readPermission' | 'writePermission' | 'permalink'>>) {
+    return await this.ky.patch(`teams/${teamPath}/notes/${noteId}`, {
+      json: options
+    }).json()
   }
 
   async deleteTeamNote (teamPath: string, noteId: string): Promise<DeleteTeamNote> {
-    await this.axios.delete<AxiosResponse>(`teams/${teamPath}/notes/${noteId}`)
+    await this.ky.delete(`teams/${teamPath}/notes/${noteId}`)
   }
 }
